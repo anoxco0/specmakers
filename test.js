7595ca7a04934f2fa5d5838900bbb3dc921ffba6
db648 <95957492+db648@users.noreply.github.com>
2022-02-25 11:29:34 +0530

    some changes made

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 const got = require('got');
 const test = require('tape');
 
-// Start the app
+//strat the app
 const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
