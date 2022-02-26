02d9b9fc83d5cecb1d6c59e466dfa1844d0ef651
Navneet Kumar <poojayadav190618@gmail.com>
2022-02-27 03:33:57 +0530

    products page ready

---
+++ controllers/products.controller.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Product = require('../models/products.model');
+const router = express.Router();
+
+router.get('/', async(req, res)=>{
+    try{
+        const product = await Product.find().lean().exec();
+        return res.send(product);
+    }
+    catch(e){
+        return res.status(500).send({message:e.message});
+    }
+})
---
+++ models/products.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    brand_name:{type:String, required:true},
+    model_name:{type:String, required:true},
+    type:{type:String, required:true},
+    frame_type:{type:String, required:true},
+    frame_shape:{type:String, required:true},
+    frame_size:{type:String, required:true},
+    price:{type:Number, required:true},
+    offer_name:{type:String, required:true},
+    specifications:{type:String, required:false},
+    color_options:[
+        {
+            color:{type:String, required:true},
+            side_image:{type:String, required:false},
+            front_image:{type:String, required:true}
+        }
+    ],
+    quantity:{type:Number, required:true},
+    wishlistCount:{type:Number, required:true},
+    purchaseCount:{type:Number, required:true},
+    avgRating:{type:Number, required:true},
+},{
+    versionKey:false,
+    timestamps:true,
+})
+
+module.exports=mongoose.model('products', productSchema)
