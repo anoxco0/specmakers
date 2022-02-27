1ca3b78ff8cc1000760b321d87fd8260adddf4c3
Navneet Kumar <poojayadav190618@gmail.com>
2022-02-27 16:16:34 +0530

    Update users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,15 +3,15 @@ const express = require('express');
 const User = require('../models/users.model')
 const router = express.Router();
 
-router.post("", async (req,res) =>{
-    try{
-        const user = await User.create(req.body);
-        return res.send(user);
-    }
-    catch(err){
-        return res.status(500).send(err.message);
-    }
-})
+// router.post("", async (req,res) =>{
+//     try{
+//         const user = await User.create(req.body);
+//         return res.send(user);
+//     }
+//     catch(err){
+//         return res.status(500).send(err.message);
+//     }
+// })
 
 router.get("", async (req,res) =>{
     try{
@@ -22,6 +22,4 @@ router.get("", async (req,res) =>{
         return res.status(500).send(err.message);
     }
 })
-
-
 module.exports = router;
