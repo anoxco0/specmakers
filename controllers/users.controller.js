0712066ebd7f45520b3823e8f015d1451d395854
db648 <95957492+db648@users.noreply.github.com>
2022-02-27 15:32:10 +0530

    updated

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,6 +3,16 @@ const express = require('express');
 const User = require('../models/users.model')
 const router = express.Router();
 
+router.post("", async (req,res) =>{
+    try{
+        const user = await User.create(req.body);
+        return res.send(user);
+    }
+    catch(err){
+        return res.status(500).send(err.message);
+    }
+})
+
 router.get("", async (req,res) =>{
     try{
         const user = await User.find().lean().exec();
