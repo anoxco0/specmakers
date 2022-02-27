de670ccfea051664e35eec4fdacda98439d73158
db648 <95957492+db648@users.noreply.github.com>
2022-02-27 15:12:21 +0530

    updated controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -13,19 +13,4 @@ router.get("", async (req,res) =>{
     }
 })
 
-// router.post('', async(req, res)=>{
-//     try{
-//         const user = await User.create({
-//             full_name:req.body.full_name,
-//             email:req.body.email,
-//             password:req.body.password,
-//             type:"customer"
-//         })
-//         console.log(user)
-//         res.end();
-//     } catch(e){
-//         return res.status(500).send({message:e.message});
-//     }
-// });
-
 module.exports = router;
