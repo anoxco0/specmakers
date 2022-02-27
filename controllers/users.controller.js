9071a218caec3ce79acdee63594a49fd99389fc0
GSudeep0204 <gudekotasudeep@gmail.com>
2022-02-27 15:19:01 +0530

    Update users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -13,5 +13,25 @@ router.get("", async (req,res) =>{
     }
 })
 
+router.get("/:id", async (req,res) =>{
+    try{
+        const user = await User.findById(req.params.id).lean().exec();
+        return res.send(user);
+    }
+    catch(err){
+        return res.status(500).send(err.message);
+    }
+})
+
+router.patch("/:id", async (req,res) =>{
+    try{
+        const user = await User.find().lean().exec();
+        return res.send(user);
+    }
+    catch(err){
+        return res.status(500).send(err.message);
+    }
+})
+
 
 module.exports = router;
